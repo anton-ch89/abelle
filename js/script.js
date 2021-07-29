cc50790e3d8cda7e67add808b7b58aef18d76483
Anton Chudinov <anton.ch89@gmail.com>
2021-07-29 22:12:40 +0500

    Добавлен футер

--- js/script.js
+++ js/script.js
@@ -1,28 +1,31 @@
 'use strict';
 
 const popupClose = document.querySelector('.popup-close'),
-popup = document.querySelector('.popup'),
+popup = document.querySelectorAll('.popup'),
 appleImg = document.querySelector('.apple-img'),
 hurmaImg = document.querySelector('.hurma-img'),
 pearImg = document.querySelector('.pear-img'),
 orangeImg = document.querySelector('.orange-img');
+console.log(popup);
 
-
-document.body.addEventListener('click', (e)=>{
+document.body.addEventListener('click', (e) => {
     let target = e.target;
-    if (target.matches('.apple-img')){
-        popup.style.display = 'block';
+
+    if (target.matches('.apple-img')) {
+        popup[0].style.display = 'block';
     }
-    if (target.matches('.hurma-img')){
-        popup.style.display = 'block';
+    if (target.matches('.hurma-img')) {
+        popup[1].style.display = 'block';
     }
-    if (target.matches('.pear-img')){
-        popup.style.display = 'block';
+    if (target.matches('.pear-img')) {
+        popup[2].style.display = 'block';
     }
-    if (target.matches('.orange-img')){
-        popup.style.display = 'block';
+    if (target.matches('.orange-img')) {
+        popup[3].style.display = 'block';
     }
-    if (target.matches('.popup') || target.matches('.popup-close')){
-        popup.style.display = 'none';
-    }
+    popup.forEach(elem => {
+        if (target.matches('.popup') || target.matches('.popup-close')) {
+            elem.style.display = 'none';
+        }
+    })
 });
